c5409471dea21e4f2605d5f2ac65022c004f2ea0
A Srinivas <asrinivass021@gmail.com>
2022-11-25 14:27:27 +0530

    minor changes

--- src/test/java/connection/Database_Full_Table.java
+++ src/test/java/connection/Database_Full_Table.java
@@ -6,39 +6,52 @@ import org.testng.annotations.Test;
 public class Database_Full_Table 
 {
 	@Test
-	public void test()
+	public void test() throws Exception
 	{
 		String url = "jdbc:mysql://localhost:3306/college";
 		String userName = "root";
 		String password = "password";
 		String query = "select * from student";
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection connection = DriverManager.getConnection(url,userName,password);
-			System.out.println("connection is successful"+url);
-			Statement st = connection.createStatement();
-			ResultSet rs = st.executeQuery(query);
-			ResultSetMetaData rsmd =  rs.getMetaData();
-			int columnsNumber = rsmd.getColumnCount();
-			while(rs.next())
-			{
-				for (int i = 1; i<=columnsNumber; i++)
-				{
-					System.out.print(rs.getString(i) + "|");
-				}
-				System.out.println();
-			}
-			st.close();
-			connection.close();
-		} 
-		catch (ClassNotFoundException e) 
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//			Connection connection = DriverManager.getConnection(url,userName,password);
+//			System.out.println("connection is successful"+url);
+//			Statement st = connection.createStatement();
+//			ResultSet rs = st.executeQuery(query);
+//			ResultSetMetaData rsmd =  rs.getMetaData();
+//			int columnsNumber = rsmd.getColumnCount();
+//			while(rs.next())
+//			{
+//				for (int i = 1; i<=columnsNumber; i++)
+//				{
+//					System.out.print(rs.getString(i) + "|");
+//				}
+//				System.out.println();
+//			}
+//			st.close();
+//			connection.close();
+//		} 
+//		catch (ClassNotFoundException e) 
+//		{
+//			e.printStackTrace();
+//		} 
+//		catch (SQLException e) 
+//		{
+//			e.printStackTrace();
+//		}
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection connection = DriverManager.getConnection(url,userName,password);
+		System.out.println("Connection is successful "+url);
+		Statement st = connection.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		String userData = "";
+		while(rs.next())
 		{
-			e.printStackTrace();
-		} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
+			userData = rs.getInt(1) + ":" + rs.getString(2);
+			System.out.println(userData);
 		}
-	
+		st.close();
+		connection.close();
 	}
 }
